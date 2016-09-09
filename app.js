28024d9512604968f0ffc6c3639c3754b6c1e59c
Philipp Hug <philipp@hug.cx>
2016-09-09 22:41:06 +0200

    switch to expressjs to fix encoding issue

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 'use strict';
 
 require('dotenv').config();
-var SwaggerConnect = require('swagger-connect');
-var app = require('connect')();
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
 var cors = require('cors');
 app.use(cors());
 
-console.log(process.env);
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerConnect.create(config, function(err, swaggerConnect) {
+SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
   // install middleware
-  swaggerConnect.register(app);
+  swaggerExpress.register(app);
 
   var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 10010;
   var host = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
